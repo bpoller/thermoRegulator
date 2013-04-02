24a418f56cd8598232e8e627ff71663f300a0263
Bert Poller <bert.poller@gmail.com>
2013-04-02 15:18:06 +0200

    Added average function

--- thermoRegulator.ino
+++ thermoRegulator.ino
@@ -10,27 +10,27 @@ const int CAPACITY = 5;
 
 /*
 Length of a period in milliseconds
-*/
+ */
 const int PERIOD = 1000;
 
 /*
  The time series backing array
-*/
+ */
 float timeSeries[CAPACITY];
 
 /*
  Time series array used for presentation and calculation purposes
-*/
+ */
 float readableTs[CAPACITY];
 
 /*
  Pointer used for time series storage. 
-*/
+ */
 int pointer = 0;
 
 /*
 System setup
-*/
+ */
 void setup(){
   initTimeSeries();
   Serial.begin(9600);
@@ -39,21 +39,26 @@ void setup(){
 
 /*
 Main processing loop
-*/
+ */
 void loop(){
 
-  put(readTemperature());
-  printTimeSeries();
+  //put(readTemperature());
+  //printTimeSeries();
 
-  delay(1000);
+  //calculateM(readableTs);
+ 
+ float test[] = {1.0, 2.0};
+ Serial.println(average(test));
+ 
+
+  delay(PERIOD);
 }
 
 
 /*
  Read the temperature.
-*/
-float readTemperature()
-{
+ */
+float readTemperature(){
   int thermistorReading = analogRead(thermistorPin);
   return thermistorReading * 0.05;
 }
@@ -64,8 +69,9 @@ float readTemperature()
 void printTimeSeries()
 {
   updateTS();
-  for (int i = 0; i < CAPACITY; i++)    {
-    Serial.print(readableTs[i]+" ");
+  for (int i = 0; i < CAPACITY; i++){
+    Serial.print(readableTs[i]);
+    Serial.print(" ");
   }
   Serial.println("");
 }
@@ -74,10 +80,8 @@ void printTimeSeries()
 /*
  Initialises time series arrays.
  */
-void initTimeSeries()
-{
-  for(int i = 0; i < CAPACITY; i++)
-  {
+void initTimeSeries(){
+  for(int i = 0; i < CAPACITY; i++){
     timeSeries[i] = 0.0;
     readableTs[i] = 0.0;
   }
@@ -100,12 +104,35 @@ void put (float value)
  */
 void updateTS(){
 
-  for(int i = 0; i < CAPACITY; i++)
-  {
+  for(int i = 0; i < CAPACITY; i++){
     int index = pointer + i +1;
     if (index > CAPACITY-1) {
       index = index -CAPACITY;
     }
     readableTs[i] = timeSeries[index];  
   } 
+}
+
+/*
+Calculate slope of tendency using least square method
+ */
+float calculateM(){
+  updateTS();
+  //float xAverage = average(xSeries(CAPACITY),CAPACITY);
+  //Serial.println(xAverage);
+  //float yAverage = average(readableTs, CAPACITY);
+}
+
+/*
+Calculates the average of a list of values.
+*/
+float average(float values[])
+{
+  float sum = 0;
+  int size = sizeof(values);
+  
+  for (int i = 0; i < size; i++){
+  sum += values[i];
+  }
+  return sum / size;
 }
