9458bd16315a6f6b51927eafa048f1378cb39b1c
Bert Poller <bert.poller@gmail.com>
2013-04-02 13:08:15 +0200

    Code clean up

--- thermoRegulator.ino
+++ thermoRegulator.ino
@@ -4,39 +4,65 @@
 int thermistorPin = A0;
 
 /*
- Time Series
+ Capacity of time series
  */
 const int CAPACITY = 5;
+
+/*
+Length of a period in milliseconds
+*/
+const int PERIOD = 1000;
+
+/*
+ The time series backing array
+*/
 float timeSeries[CAPACITY];
+
+/*
+ Time series array used for presentation and calculation purposes
+*/
 float readableTs[CAPACITY];
+
+/*
+ Pointer used for time series storage. 
+*/
 int pointer = 0;
 
+/*
+System setup
+*/
 void setup(){
   initTimeSeries();
   Serial.begin(9600);
 }
 
+
+/*
+Main processing loop
+*/
 void loop(){
 
   put(readTemperature());
   printTimeSeries();
 
-  delay(1000); //just here to slow down the output for easier reading
+  delay(1000);
 }
 
+
+/*
+ Read the temperature.
+*/
 float readTemperature()
 {
   int thermistorReading = analogRead(thermistorPin);
   return thermistorReading * 0.05;
 }
 
-
 /*
-  Prints the contents of the buffer without modifying its contents.
+  Prints the current time series without modifying its contents.
  */
 void printTimeSeries()
 {
-
   updateTS();
   for (int i = 0; i < CAPACITY; i++)    {
     Serial.print(readableTs[i]+" ");
@@ -44,15 +70,10 @@ void printTimeSeries()
   Serial.println("");
 }
 
-void put (float value)
-{
-  pointer++;
-  if(pointer % CAPACITY == 0){ 
-    pointer = 0;
-  } 
-  timeSeries[pointer] = value;
-}
 
+/*
+ Initialises time series arrays.
+ */
 void initTimeSeries()
 {
   for(int i = 0; i < CAPACITY; i++)
@@ -62,13 +83,29 @@ void initTimeSeries()
   }
 }
 
+/*
+ Put value in round-robin fashion into the time series array 
+ */
+void put (float value)
+{
+  pointer++;
+  if(pointer % CAPACITY == 0){ 
+    pointer = 0;
+  } 
+  timeSeries[pointer] = value;
+}
+
+/*
+ Copies the current time series backing array into a presentable array.
+ */
 void updateTS(){
-  
+
   for(int i = 0; i < CAPACITY; i++)
   {
     int index = pointer + i +1;
-    if (index > CAPACITY-1) {index = index -CAPACITY;}
+    if (index > CAPACITY-1) {
+      index = index -CAPACITY;
+    }
     readableTs[i] = timeSeries[index];  
-}
-  
+  } 
 }
