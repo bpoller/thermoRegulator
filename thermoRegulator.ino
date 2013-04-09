02a8defbda71599105313331b1a7579086bf4bdf
Bert Poller <bert.poller@gmail.com>
2013-04-09 12:28:17 +0200

    Added jsonp web service

--- thermoRegulator.ino
+++ thermoRegulator.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <avr/pgmspace.h>
+#include <string.h>
 
 //Used to store large strings in progmem.
 char p_buffer[80];
@@ -54,6 +55,11 @@ float readableTs[CAPACITY][2];
  */
 int pointer = 0;
 
+/*
+ JSONP callback function.
+*/
+String callback = "";
+
 
 
 /*
@@ -93,6 +99,7 @@ void serveWebRequests()
   if (client) {
     Serial.println(P("new client"));
     bufferRequest(client);
+    parseReceivedRequest();
     writeResponse(client);
 
     // Give the web browser time to receive the data
@@ -123,6 +130,42 @@ void bufferRequest(EthernetClient client)
           break;
     }
   }
+}
+
+void parseReceivedRequest()
+{
+  Serial.println("in parseReceivedRequest");
+  Serial.println(buffer);
+  
+  //Received buffer contains "GET /cmd/param1/param2 HTTP/1.1".  Break it up.
+ 
+  char* slash1;
+  char* slash2;
+  char* slash3;
+  char* space2;
+  
+  slash1 = strstr(buffer, "/") + 1; // Look for first slash
+  slash2 = strstr(slash1, "/") + 1; // second slash
+  slash3 = strstr(slash2, "/") + 1; // third slash
+  space2 = strstr(slash2, " ") + 1; // space after second slash (in case there is no third slash)
+  if (slash3 > space2) slash3=slash2;
+
+  Serial.println(slash1);
+  Serial.println(slash2);
+  Serial.println(slash3);
+  Serial.println(space2);
+  
+  // strncpy does not automatically add terminating zero, but strncat does! So start with blank string and concatenate.
+  /*cmd[0] = 0;
+  param1[0] = 0;
+  param2[0] = 0;
+  strncat(cmd, slash1, slash2-slash1-1);
+  strncat(param1, slash2, slash3-slash2-1);
+  strncat(param2, slash3, space2-slash3-1);
+  
+  PrintString("cmd",cmd);
+  PrintString("param1",param1);
+  PrintString("param2",param2);*/
 }
 
 /*
@@ -183,8 +226,8 @@ void writeWebPage(EthernetClient client)
   readTimeSeries();
   float m = calculateM();
   float n = calculateN(m);
-
-  client.print(P("{tempHisto:"));
+  client.print(callback);
+  client.print(P("({tempHisto:"));
   printTempHisto(client);
   client.print(P(", tempNow:"));
   client.print(readTemperature());
@@ -198,7 +241,7 @@ void writeWebPage(EthernetClient client)
   client.print(m);
   client.print(P(",n:"));
   client.print(n);
-  client.print(P("}"));
+  client.print(P("});"));
 }
 
 String printTempHisto(EthernetClient client){
