5b1c892cdf38a998eed226954772242270842a09
Bert Poller <bert.poller@gmail.com>
2013-04-10 09:42:48 +0200

    working on web page

--- thermoRegulator.ino
+++ thermoRegulator.ino
@@ -56,9 +56,9 @@ float readableTs[CAPACITY][2];
 int pointer = 0;
 
 /*
- JSONP callback function.
+ JSONP callback function name.
 */
-String callback = "";
+char callbackFunction[20];
 
 
 
@@ -134,38 +134,14 @@ void bufferRequest(EthernetClient client)
 
 void parseReceivedRequest()
 {
-  Serial.println("in parseReceivedRequest");
-  Serial.println(buffer);
-  
-  //Received buffer contains "GET /cmd/param1/param2 HTTP/1.1".  Break it up.
- 
-  char* slash1;
-  char* slash2;
-  char* slash3;
-  char* space2;
-  
-  slash1 = strstr(buffer, "/") + 1; // Look for first slash
-  slash2 = strstr(slash1, "/") + 1; // second slash
-  slash3 = strstr(slash2, "/") + 1; // third slash
-  space2 = strstr(slash2, " ") + 1; // space after second slash (in case there is no third slash)
-  if (slash3 > space2) slash3=slash2;
-
-  Serial.println(slash1);
-  Serial.println(slash2);
-  Serial.println(slash3);
-  Serial.println(space2);
-  
+  char* callbackString = strstr(buffer, "?callback=") + 10; // Look for question mark
+  char* space = strstr(callbackString, " ") + 1; // space after command
+   
   // strncpy does not automatically add terminating zero, but strncat does! So start with blank string and concatenate.
-  /*cmd[0] = 0;
-  param1[0] = 0;
-  param2[0] = 0;
-  strncat(cmd, slash1, slash2-slash1-1);
-  strncat(param1, slash2, slash3-slash2-1);
-  strncat(param2, slash3, space2-slash3-1);
+  callbackFunction[0] = 0;
   
-  PrintString("cmd",cmd);
-  PrintString("param1",param1);
-  PrintString("param2",param2);*/
+  //copy function name into callback function
+  strncat(callbackFunction, callbackString, space-callbackString-1);
 }
 
 /*
@@ -226,20 +202,20 @@ void writeWebPage(EthernetClient client)
   readTimeSeries();
   float m = calculateM();
   float n = calculateN(m);
-  client.print(callback);
-  client.print(P("({tempHisto:"));
+  client.print(callbackFunction);
+  client.print(P("({\"tempHisto\":"));
   printTempHisto(client);
-  client.print(P(", tempNow:"));
+  client.print(P(", \"tempNow\":"));
   client.print(readTemperature());
-  client.print(P(",setPoint:"));
+  client.print(P(",\"setPoint\":"));
   client.print(SET_POINT);
-  client.print(P(",period:"));
+  client.print(P(",\"period\":"));
   client.print(PERIOD);
-  client.print(P(",capacity:"));
+  client.print(P(",\"capacity\":"));
   client.print(CAPACITY);
-  client.print(P(",m:"));
+  client.print(P(",\"m\":"));
   client.print(m);
-  client.print(P(",n:"));
+  client.print(P(",\"n\":"));
   client.print(n);
   client.print(P("});"));
 }
